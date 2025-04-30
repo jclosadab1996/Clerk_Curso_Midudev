f354ae4b29b3d82862668441b739d320c6f46afd
Juan Camilo Losada Bultaif <92903961+jclosadab1996@users.noreply.github.com>
2025-04-29 23:27:36 -0500

    Change the style of appereance

--- Clerk_astro/astro.config.mjs
+++ Clerk_astro/astro.config.mjs
@@ -2,12 +2,19 @@
 import { defineConfig } from "astro/config";
 import node from "@astrojs/node";
 import clerk from "@clerk/astro";
+import { neobrutalism } from "@clerk/themes";
 
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [clerk()],
+  integrations: [
+    clerk({
+      appearance: {
+        baseTheme: neobrutalism,
+      },
+    }),
+  ],
   vite: {
     plugins: [tailwindcss()],
   },
