7adce412882dfa70e86af0e72d1a1b9d1aa67c3a
Juan Camilo Losada Bultaif <92903961+jclosadab1996@users.noreply.github.com>
2025-04-29 23:21:36 -0500

    Finish astro auth with clerk

---
+++ Clerk_astro/src/middleware.ts
@@ -0,0 +1,11 @@
+import { clerkMiddleware, createRouteMatcher } from "@clerk/astro/server";
+
+const isProtectedRoute = createRouteMatcher(["/retos(.*)"]);
+
+export const onRequest = clerkMiddleware((auth, context) => {
+  const { userId, redirectToSignIn } = auth();
+
+  if (isProtectedRoute(context.request) && !userId) {
+    return redirectToSignIn();
+  }
+});
